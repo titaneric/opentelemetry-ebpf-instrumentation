e427d2ed854f5d212aca96fdd6dc69be7d4a0877
titaneric <chenyihuang001@gmail.com>
2025-06-21 06:03:58 +0000

    add early bound check and json_value_eq helper function

--- bpf/gotracer/go_nethttp.c
+++ bpf/gotracer/go_nethttp.c
@@ -1369,8 +1369,8 @@ int beyla_read_jsonrpc_method(struct pt_regs *ctx) {
     __builtin_memcpy(body_buf, body_map_buf, sizeof(body_buf));
     if (is_jsonrpc2_body((const unsigned char *)body_buf, sizeof(body_buf))) {
         unsigned char method_buf[JSONRPC_METHOD_BUF_SIZE] = {};
-        u32 method_len =
-            extract_jsonrpc2_method((const unsigned char *)body_buf, sizeof(body_buf), method_buf);
+        u32 method_len = extract_jsonrpc2_method(
+            (const unsigned char *)body_buf, sizeof(body_buf), method_buf, sizeof(method_buf));
         if (method_len > 0) {
             bpf_dbg_printk("JSON-RPC method: %s", method_buf);
             read_go_str_n("JSON-RPC method",
