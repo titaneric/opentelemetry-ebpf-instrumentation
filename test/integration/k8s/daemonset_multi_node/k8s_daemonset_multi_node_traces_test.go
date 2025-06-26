c14bb4392868f4da84d2524ab58ac2a1ca249940
titaneric <chenyihuang001@gmail.com>
2025-06-26 17:39:27 +0000

    add k8s integration test for jsonrpc server

--- test/integration/k8s/daemonset_multi_node/k8s_daemonset_multi_node_traces_test.go
+++ test/integration/k8s/daemonset_multi_node/k8s_daemonset_multi_node_traces_test.go
@@ -20,19 +20,20 @@ import (
 // For the this scenario we run two worker nodes, with the following structure:
 //   - worker 1:
 //     testserver [go app] port: 8080
+//     testserver [go jsonrpc app] port: 8088
 //   - worker 2:
 //     pythonserver [python app] port: 8083
 //     ruby on rails [ruby app] port: 3040
 //
 // The call flow is as follows:
 //
-//	testserver [/gotracemetoo] -> Python server [/tracemetoo] -> Ruby server [/users]
+//	testserver [/gotracemetoo] -> go jsonrpc [/jsonrpc] -> Python server [/tracemetoo] -> Ruby server [/users]
 //
 // They should all have the same traceID. Across nodes the TCP context propagation (OTEL_EBPF_BPF_CONTEXT_PROPAGATION)
 // connects the dots, while on the same node, the networking is optimized and we rely on black-box context propagation to
 // connect the services.
 func TestMultiNodeTracing(t *testing.T) {
-	feat := features.New("Beyla is able to generate distributed traces go->python->ruby").
+	feat := features.New("Beyla is able to generate distributed traces go->go jsonrpc->python->ruby").
 		Assess("it sends connected traces for all services",
 			func(ctx context.Context, t *testing.T, _ *envconf.Config) context.Context {
 				var trace jaeger.Trace
@@ -68,6 +69,13 @@ func TestMultiNodeTracing(t *testing.T) {
 					}, trace.Processes[parent.ProcessID].Tags)
 					require.Empty(t, sd, sd.String())
 
+					// Check the information of the Go jsonrpc span
+					res = trace.FindByOperationName("Arith.T /jsonrpc", "server")
+					require.Len(t, res, 1)
+					parent = res[0]
+					require.NotEmpty(t, parent.TraceID)
+
+					require.Equal(t, traceID, parent.TraceID)
 					// Check the information of the Python span
 					res = trace.FindByOperationName("GET /tracemetoo", "server")
 					require.Len(t, res, 1)
